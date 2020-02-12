d89304ef8b06f2c92428a774804a774eccac2f40
Manpreet Singh <manpreet.singh@cineplex.com>
2020-02-11 23:21:06 -0500

    first foundation

---
+++ YoYoFramework/YoYoFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  YoYoFramework
+//
+//  Created by Manpreet Singh on 2020-02-11.
+//  Copyright © 2020 Manpreet Singh. All rights reserved.
+//
+
+import Foundation
+
+class Service {
+
+    init() {}
+
+    public static func doSomething() {
+        print("yo yo !")
+    }
+}
---
+++ YoYoFramework/YoYoFramework/YoYoFramework.h
@@ -0,0 +1,19 @@
+//
+//  YoYoFramework.h
+//  YoYoFramework
+//
+//  Created by Manpreet Singh on 2020-02-11.
+//  Copyright © 2020 Manpreet Singh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YoYoFramework.
+FOUNDATION_EXPORT double YoYoFrameworkVersionNumber;
+
+//! Project version string for YoYoFramework.
+FOUNDATION_EXPORT const unsigned char YoYoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YoYoFramework/PublicHeader.h>
+
+
